06a0f1762d7d184f1f3829cbb4b4753310de7e44
Darwin <floresjack74@gmail.com>
2025-03-31 21:12:08 -0500

    ajustes por segunda parte para vercel

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const conexion = require("../config/conexion");
-const { error } = require("console");
 const router = express.Router();
 
-//ruta para obtener todos los platos de la bd railway
-router.get("/",(req,res)=>{
-    conexion.query("select * from platos",(error,resultados)=>{
-        if(error){
-            res.status(500).json({error:"error al obtener usuarios"});
+// Ruta para obtener todos los platos de la BD railway
+router.get("/", (req, res) => {
+    conexion.query("SELECT * FROM platos", (error, resultados) => {
+        if (error) {
+            res.status(500).json({ error: "Error al obtener platos" });
             return;
         }
         res.json(resultados);
     });
 });
 
-router.post("/",(req,res)=>{
-    const {plato,precio,descripcion,categoria} = req.body;
-    const valores = [plato,precio,descripcion,categoria];
-    const consulta = "insert into platos(plato,precio,descripcion,categoria) values(?,?,?,?)";
-    conexion.query(consulta,valores,(error,resultado)=>{
-        if(error){
-            return res.status(400).json({error:"ocurrio un error al insertar los datos"})
+// Ruta para insertar un nuevo plato
+router.post("/", (req, res) => {
+    const { plato, precio, descripcion, categoria } = req.body;
+    const valores = [plato, precio, descripcion, categoria];
+    const consulta = "INSERT INTO platos(plato, precio, descripcion, categoria) VALUES(?, ?, ?, ?)";
+    conexion.query(consulta, valores, (error, resultado) => {
+        if (error) {
+            return res.status(400).json({ error: "Ocurrió un error al insertar los datos" });
         }
-        return res.status(200).json({mensaje:"resultado agregado exitosamente"})
-    })
-})
-//exportamos la ruta
+        return res.status(200).json({ mensaje: "Plato agregado exitosamente" });
+    });
+});
+
+// Exportamos la ruta
 module.exports = router;
--- servidor.js
+++ servidor.js
@@ -4,14 +4,13 @@ const cors = require("cors");
 const usuariosRoutes = require("./routes/usuarios");
 const { json } = require("body-parser");
 
-const app = express()
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-//usamos las rutas de usuarios
-app.use('/usuarios',usuariosRoutes);
+// Agregar el prefijo /api a las rutas de usuarios
+app.use('/api/usuarios', usuariosRoutes);
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log("servidor corriendo en el puerto 3000");
-})
-
+});
