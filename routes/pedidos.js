33315aa11736e81f4ebc2ec8951209c31c219e46
Darwin <floresjack74@gmail.com>
2025-04-05 22:18:20 -0500

    agregando rutas de edicion para los pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -13,4 +13,36 @@ router.get("/",(req,res)=>{
     })
 })
 
+router.post("/entregado",(req,res)=>{
+    const {id_mesa} = req.body
+    const consulta = "update pedidos set estado = 'Entregado' where id_pedido = ?"
+    conexion.query([id_mesa],(error,resultado)=>{
+        if(error){
+            res.status(500).json({error:"ocurrio un error al editar el campo de estado"})
+        }
+        res.status(200).json({mensaje:"exito al editar el campo de estado"})
+    })
+})
+
+router.post("/enCamino",(req,res)=>{
+    const {id_mesa} = req.body;
+    const consulta = "update pedidos set estado = 'En Camino' where id_pedido = ?";
+    conexion.query([id_mesa],(error,resultado)=>{
+        if(error){
+            res.status(500).json({error:"ocurrio un error al actualizar los datos"})
+        }
+        res.status(200).json({mensaje:"datos actualizados correctamente, el plato esta en camino"})
+    })
+})
+
+router.post("/enCocina",(req,res)=>{
+    const {id_mesa} = req.body
+    const consulta = "update pedidos set estado = 'En Cocina' where id_pedido = ?";
+    conexion.query([id_mesa],(error,resultado)=>{
+        if(error){
+            res.status(500).json({error:"error al actualizar los datos a En Cocina"})
+        }
+        res.status(200).json({mensaje:"datos actualizados correctamente, el plato esta cocina"})
+    })
+})
 module.exports = router;
